0267838fe6d4898dfdca97641c58c1d46e5be7c5
mdromi <mdromi14@gmail.com>
2023-10-15 11:55:59 -0400

    Remove all content

--- README.md
+++
@@ -1,2 +0,0 @@
-# exp-blog-frontend
-A scalable react frontend system for a multi-vendor blog application enabling diverse content publication and engagement.
--- src/store/modules/theme/action/themeAction.ts
+++
@@ -1,9 +0,0 @@
-import { SET_THEME } from "../ThemeTypes";
-
-export const setTheme = (theme: String) => {
-  console.log("theme", theme);
-  return {
-    type: SET_THEME,
-    payload: theme,
-  };
-};
